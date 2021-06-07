0044827396e21e5c1699e2f7c91a5d2ae693fcb7
sharmay334 <sharmay334@gmail.com>
2021-06-07 19:44:54 +0530

    Delete NokiaTask directory

--- NokiaTask/target/EMSMAIN-0.0.1-SNAPSHOT/WEB-INF/classes/db/migration/V1621083438__ddl_add_access_level.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `ems_t_user_child_menu_mapping` 
-ADD COLUMN `access_level` ENUM('VIEWER','EDITOR','MASTER') DEFAULT 'VIEWER' NULL AFTER `menu_id`; 
--- NokiaTask/target/EMSMAIN-0.0.1-SNAPSHOT/WEB-INF/classes/db/migration/V1621773542__new_cols_added_in_user_master.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `ems_t_user_master` ADD COLUMN `org_name` VARCHAR(255) NOT NULL AFTER `full_name`; 
-UPDATE `ems_t_user_master` SET `org_name` = 'EMS INC.' WHERE `id` = '1'; 
-ALTER TABLE `ems_t_user_master` ADD COLUMN `hierarchy_string` TEXT NOT NULL AFTER `parent_user_id`;  
- UPDATE `ems_t_user_master` SET `hierarchy_string` = '#1' WHERE `id` = '1'; 
